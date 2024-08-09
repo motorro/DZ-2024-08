d02d8b0f083fce23c53ad55d5535c25607c7c6f5
Motorro <motorro@gmail.com>
2024-08-09 11:48:45 +0200

    Dynamic menu

--- app/src/main/java/ru/otus/homework/MainActivity.kt
+++ app/src/main/java/ru/otus/homework/MainActivity.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import android.view.View
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
 import androidx.annotation.MenuRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.ListPopupWindow
 import androidx.appcompat.widget.PopupMenu
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import ru.otus.homework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         with(binding) {
             buttonStatic.setOnClickListener { v -> showStaticMenu(v, R.menu.menu_static) }
+            buttonDynamic.setOnClickListener { v -> showDynamicMenu(v, R.menu.menu_static) }
         }
     }
 
@@ -39,6 +39,30 @@ class MainActivity : AppCompatActivity() {
         }
         // Show the popup menu.
         popup.show()
+    }
+
+    private fun showDynamicMenu(v: View, @MenuRes menuRes: Int) {
+        val listPopupWindow = ListPopupWindow(
+            this,
+            null,
+            androidx.appcompat.R.attr.listPopupWindowStyle
+        )
+        listPopupWindow.anchorView = v
+
+        val items = listOf(
+            getString(R.string.option_1),
+            getString(R.string.option_2),
+            getString(R.string.option_3)
+        )
+        val adapter = ArrayAdapter(this, R.layout.item_menu, items)
+        listPopupWindow.setAdapter(adapter)
+
+        listPopupWindow.setOnItemClickListener { _, _, position: Int, _ ->
+            Log.i(TAG, "Menu item clicked: ${items[position]}")
+            listPopupWindow.dismiss()
+        }
+        // Show the popup menu.
+        listPopupWindow.show()
     }
 
     companion object {
